3cdb7c350df806dabc385c7860cdd32994b0767e
Sharjeel Afridi <52808124+Sharjeel-Afridi@users.noreply.github.com>
2023-07-22 00:51:28 +0530

    new card function begins

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 let firstCard = 8;
 let secondCard = 9;
+
 let hasBlackJack = false;
 let sum = firstCard + secondCard;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+let startBtn = document.querySelector(".start-btn")
 
-if (sum < 21){
-    message = "Do you want to draw a new card? "
-    
 
-} else if (sum === 21) {
-    message = "Wohoo! You've got Black Jack!"
-    
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game!"
-    
-    isAlive = false;
+function startGame() {
+    renderGame()
+}
+function renderGame(){
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    startBtn.textContent = "Restart"
+    if (sum < 21){
+        message = "Do you want to draw a new card? "
+        
+
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Black Jack!"
+        
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!"
+        
+        isAlive = false;
+    }
+    messageEl.textContent = message
 }
 
-console.log(message)
+function newCard() {
+    sum += thirdCard
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard + ", " + thirdCard
+    sumEl.textContent = "Sum: " + sum
+    renderGame()
+}
